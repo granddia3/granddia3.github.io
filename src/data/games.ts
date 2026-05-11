7d8c82a082bc461c200ccfb070928ef84f13f5ad
peak diamond 2 <aedan.l.kennison@gmail.com>
2026-05-11 22:22:27 +0000

    Added more games

--- src/data/games.ts
+++ src/data/games.ts
@@ -10,10 +10,10 @@ export interface Game {
 export const games: Game[] = [
   {
     id: 'game-1',
-    title: 'tribals',
+    title: 'tribals(NOT WORKING)',
     description: '',
     url: 'https://script.google.com/macros/s/AKfycbyQAmL7_9B9SGFKb9xwOh661dzn-kzNjuLwyDPSg0hY6WD79s2lwAny9uHhvRGRDqLq/exec',
-    thumbnail: 'https://picsum.photos/seed/arcade/600/400',
+    thumbnail: 'https://img.craiyon.com/2026-05-11/p4enqAwJQAOoXs4TOVu_Iw.webp',
     category: 'Action'
   },
   {
@@ -26,18 +26,42 @@ export const games: Game[] = [
   },
   {
     id: 'game-3',
-    title: 'Assault Bots',
+    title: 'Assault Bots(NOT WORKING)',
     description: '',
     url: 'https://script.google.com/macros/s/AKfycbz5GtoRAR-e_NYMEGdV33FLU03F3DlGqW34qEkkEzGC5D8e7rvDjJs4S11jaH5_J_z7/exec',
-    thumbnail: 'https://picsum.photos/seed/strategy/600/400',
+    thumbnail: 'https://img.craiyon.com/2026-05-11/p4enqAwJQAOoXs4TOVu_Iw.webp',
     category: 'Strategy'
   },
   {
     id: 'game-4',
-    title: 'Bloxd',
+    title: 'Bloxd(NOT WORKING)',
     description: '',
     url: 'https://script.google.com/macros/s/AKfycbwAsvXTh7XurBCTIbcTw-5reuXukTR533fvSlll-toib_7SG_JYklsitXLgSu9mLIolbA/exec',
-    thumbnail: 'https://picsum.photos/seed/adventure/600/400',
+    thumbnail: 'https://img.craiyon.com/2026-05-11/p4enqAwJQAOoXs4TOVu_Iw.webp',
     category: 'Adventure'
+  },
+  {
+    id: 'game-5',
+    title: 'Tag',
+    description: '',
+    url: 'https://mc0825.github.io/g69/class-633/',
+    thumbnail: 'https://taggame.io/data/image/options/tag-game-banner-tgio.png',
+    category: 'Multiplayer'
+  },
+  {
+    id: 'game-6',
+    title: 'Getaway Shootout',
+    description: '',
+    url: 'https://mc0825.github.io/g9/class-479/',
+    thumbnail: 'https://getawayshootoutonline.github.io/images/getaway-shootout.png',
+    category: 'Multiplayer'
+  },
+  {
+    id: 'game-7',
+    title: 'Animals Volleyball',
+    description: '',
+    url: 'https://mc0825.github.io/g66/class-847/',
+    thumbnail: 'https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=1200,height=1200,fit=cover,f=png/966cec3b19cedf7dd72f93419f13268c/animals-volleyball-logo.png',
+    category: 'Sports'
   }
 ];
